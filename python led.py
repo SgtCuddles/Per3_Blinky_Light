3f109a6441829d3c4fe674fadd074f6a5c546659
Zachary Daly <zdaly3183@gmail.com>
2018-05-04 13:37:41 -0700

    Now with extra protien

--- python led.py
+++ python led.py
@@ -8,50 +8,53 @@ def choose_Blink(on_time, off_time):
 	led.off()
 	sleep(off_time)
 
+def Morse():
+	continu = True
+	while continu:
+		for i in range(0, 2):
+			choose_Blink(0.25,0.5)
+			choose_Blink(0.5,0.5)
+			choose_Blink(0.25,0.5)
+			choose_Blink(0.5,1)
+			
+			choose_Blink(0.5,0.5)
+			choose_Blink(0.5,0.5)
+			choose_Blink(0.5,1)
+			
+			choose_Blink(0.25,0.5)
+			choose_Blink(0.50,0.5)
+			choose_Blink(0.25,1)
+			
+			choose_Blink(0.25,1)
+			
+			choose_Blink(0.5,0.5)
+			choose_Blink(0.5,2)
+			
+			
+			choose_Blink(0.25,0.5)
+			choose_Blink(0.25,1)
+			
+			choose_Blink(0.25,0.5)
+			choose_Blink(0.5,0.5)
+			choose_Blink(0.5,0.5)
+			choose_Blink(0.25,0.5)
+			
+			choose_Blink(0.25,0.5)
+			choose_Blink(0.25,0.5)
+			choose_Blink(0.25,1)
+			
+			choose_Blink(0.25,0.5)
+			choose_Blink(0.25,0.5)
+			choose_Blink(0.5,1)
+			
+			choose_Blink(0.5,0.5)
+			choose_Blink(0.5,1)
 		
-#.-.. --- .-. . -- / .. .--. ... ..- --
-continu = True
-while continu:
-	for i in range(0, 2):
-		choose_Blink(0.25,0.5)#l
-		choose_Blink(0.5,0.5)
-		choose_Blink(0.25,0.5)
-		choose_Blink(0.5,1)
-		
-		choose_Blink(0.5,0.5)#o
-		choose_Blink(0.5,0.5)
-		choose_Blink(0.5,1)
-		
-		choose_Blink(0.25,0.5)#r
-		choose_Blink(0.50,0.5)
-		choose_Blink(0.25,1)
-		
-		choose_Blink(0.25,1)#e
-		
-		choose_Blink(0.5,0.5)#m
-		choose_Blink(0.5,2)
-		
-		
-		choose_Blink(0.25,0.5)#i
-		choose_Blink(0.25,1)
-		
-		choose_Blink(0.25,0.5)#p
-		choose_Blink(0.5,0.5)
-		choose_Blink(0.5,0.5)
-		choose_Blink(0.25,0.5)
-		
-		choose_Blink(0.25,0.5)#s
-		choose_Blink(0.25,0.5)
-		choose_Blink(0.25,1)
-		
-		choose_Blink(0.25,0.5)#u
-		choose_Blink(0.25,0.5)
-		choose_Blink(0.5,1)
-		
-		choose_Blink(0.5,0.5)#m
-		choose_Blink(0.5,1)
-	
-	quit = input("Do you want to quit? (type yes to quit)")
-	if quit == 'yes':
-		continu = False
-	
+		quit = str(input("Do you want to quit? (type yes to quit)"))
+		if quit == 'yes':
+			continu = False
+			
+def Main():
+	choices = {'morse':Morse, 'Manual':steady,'repeat':Repeat}
+	choice = input("Available Behaviors:\n\t-Morse\n\t-Manual\n\t-Repeat").lower
+	choices[choice]()
