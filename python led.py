bb904eb3058c2add39c9d252355d416eb01c3ef4
Zachary Daly <zdaly3183@gmail.com>
2018-05-04 12:48:35 -0700

    This should ask for durations

--- python led.py
+++ python led.py
@@ -2,8 +2,20 @@ from gpiozero import LED
 from time import sleep
 led = LED(2)
 
-while True:
-	led.on()
-	sleep(1)
-	led.off()
-	sleep(1)
+#while True:
+#	led.on()
+#	sleep(1)
+#	led.off()
+#	sleep(1)
+	
+def choose_Blink(on_time, off_time):
+	while True:
+		led.on()
+		sleep(on_time)
+		led.off()
+		sleep(off_time)
+
+		
+duration1 = float(input("How many seconds should the LED stay on?\n"))
+duration2 = float(input("How many seconds should the LED stay off?\n"))
+choose_Blink():
